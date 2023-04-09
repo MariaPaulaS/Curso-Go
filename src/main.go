f42025623b73c05ebccbda2e8504409a72d55843
Maria <mariathecharmix@hotmail.com>
2023-04-08 20:30:39 -0500

    Manejo de funciones

--- src/main.go
+++ src/main.go
@@ -5,10 +5,10 @@ import (
 	"math"
 )
 
-func main() {
+const pi float64 = 3.14
+const pi2 = 3.14159
 
-	const pi float64 = 3.14
-	const pi2 = 3.14159
+func main() {
 
 	fmt.Println("pi: ", pi)
 	fmt.Println("pi2: ", pi2)
@@ -78,20 +78,13 @@ func main() {
 	//Areas
 	//Rectangulo - Trapecio - Circulo
 
-	baseRectangulo := 12
-	alturaRectangulo := 10
-	resultArea := baseRectangulo * alturaRectangulo
-
+	resultArea := rectangleArea(12, 10)
 	fmt.Println("Area del rectangulo", resultArea)
 
-	primeraLongitud := 13
-	segundaLongitud := 20
-
-	resultArea = ((primeraLongitud + segundaLongitud) * altura) / 2
+	resultArea = trapeziumArea(13, 20, altura)
 	fmt.Println("Area del trapecio", resultArea)
 
-	radio := 5.0
-	resultAreaFloat := pi2 * math.Pow(radio, 2)
+	resultAreaFloat := circleArea(5.0)
 
 	fmt.Println("Area del circulo", resultAreaFloat)
 
@@ -119,6 +112,44 @@ func main() {
 	fmt.Println(message)
 
 	//Obtener tipo de dato de una variable
-	fmt.Printf("Hello data type: %T", hello)
+	fmt.Printf("Hello data type: %T \n", hello)
+
+	//Funciones
+	printMessage("Hello world function")
+	tripeArgument(1, 2, "Hola")
+	fmt.Println("Return value", returnValue(2))
+
+	value1, _ := doubleReturn(5)
+	fmt.Println("Value1:", value1)
+
+}
+
+func printMessage(message string) {
+	fmt.Println(message)
+}
+
+func tripeArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+
+}
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
+func rectangleArea(rectangleBase, heightBase int) int {
+	return rectangleBase * heightBase
+}
+
+func trapeziumArea(firstLenght, secondLenght, height int) int {
+	return ((firstLenght + secondLenght) * height) / 2
+}
+
+func circleArea(radio float64) float64 {
+	return pi2 * math.Pow(radio, 2)
 
 }
